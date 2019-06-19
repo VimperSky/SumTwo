99e3274604c5af3d8e03570ef73fafe6ce59b565
ImperialMan <VladImpersky@Gmail.com>
2019-06-19 16:34:01 +0300

    Add files

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var sum = Convert.ToInt32(args[0]) + Convert.ToInt32(args[1]);
+                Console.WriteLine(sum.ToString());
+            }
+            catch
+            {
+                Console.WriteLine("Wrong input!");
+            }
+        }
+    }
+}
